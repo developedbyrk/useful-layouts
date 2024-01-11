273ae18efd91d08f96cd7281c693e0c0f44d1dc0
Rajesh Kumar <84664016+developedbyrk@users.noreply.github.com>
2024-01-11 16:54:28 +0530

    initial code upload

---
+++ msg-with-flex/app.js
@@ -0,0 +1,29 @@
+function insetSuggestedMessage() {
+    let newElement = document.createElement("div");
+    let newElementText = document.createTextNode('Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quaerat sed facere voluptate doloremque. Commodi, in? Mollitia voluptatibus quos id tenetur omnis veniam aut ea.');
+    newElement.classList.add('added-content')
+    newElement.appendChild(newElementText);
+
+    document.querySelector('.suggested-message-content').appendChild(newElement);
+
+    let ht = document.querySelector('.suggested-message').offsetHeight;
+    document.querySelector(".output").textContent
+        = ht + "px";
+}
+
+function deleteSuggestedMessage() {
+    let removableItem = document.querySelector('.added-content:last-child');
+    removableItem.remove();
+}
+
+function toggleSuggestedMessage() {
+    document.querySelector('.suggested-message').classList.toggle('toggle-message');
+}
+
+/*
+function getHeight() {
+    let ht = document.querySelector('.suggested-message').offsetHeight;
+    document.querySelector(".output").textContent
+        = ht + "px";
+}
+*/
